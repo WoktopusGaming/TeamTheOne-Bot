a0a60a120eef862b7bdbe7feaf6f7f4d361a867f
WoktopusGaming <116921742+WoktopusGaming@users.noreply.github.com>
2025-05-09 07:05:37 -0400

    removing some features in indexer + changing port to 5000 (unused)
    side note: indexer, templates and static files might no longer be available
    indexer will instead return a "send" value for all copies of the TeamTheBot github repository

--- dexer.py
+++ dexer.py
@@ -1,36 +1,11 @@
-from flask import render_template, Flask as Quart, send_from_directory
+from flask import render_template, Flask as Quart #, send_from_directory
 #from quart import render_template, Quart, send_from_directory - soon
 from threading import Thread
 from random import randrange
 import os
-import traceback
-import socket
-from contextlib import closing
-   
-def check_socket(host, port):
-  try:
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-        if sock.connect_ex((host, port)) == 0:
-            return 2
-        else:
-            return 1
-  except Exception as e:
-    print("Socket couldn't be checked. (Error TTO-009)")
-    traceback.format_exc()
-    return 0
 
 #app definition
 app = Quart(__name__)
-
-
-def randport():
-  num = check_socket('0.0.0.0', 8080)
-  if num == 2:
-    return randrange(3000, 9000)
-  elif num == 1:
-    return 8080
-  else:
-    return randrange(7900, 8100)
 
 #site route
 @app.route('/')
@@ -39,7 +14,7 @@ def home():
 
 #run site
 def run():
-    app.run(host='0.0.0.0', port=randport())
+    app.run(host='0.0.0.0', port='5000')
 
 
 #dexer definition (used in main.py to run website)
